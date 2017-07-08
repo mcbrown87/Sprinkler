b57a49fc67c3a13aabc6acd88bbd434f5c5e8dc9
mcbrown87 <michael.c.brown87@gmail.com>
2017-07-08 10:30:04 -0400

    Added a mock valve class. Added a means to measure how long the valve has been open.

--- mockValve.py
+++ mockValve.py
@@ -1,32 +1,26 @@
-import RPi.GPIO as GPIO
 import datetime
 from stopwatch import Stopwatch
 
-
 class Valve:
-
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(7, GPIO.OUT)
-	GPIO.output(7, GPIO.HIGH)
-
 	def __init__(self):
+		self._isOpen = False
 		self._stopwatch = None
 
 	def IsOpen(self):
-		return not GPIO.input(7)
+		return self._isOpen
 
 	def Open(self):
 		if(self.IsOpen()):
 			return
 
-		GPIO.output(7, GPIO.LOW)
+		self._isOpen = True
 		self._stopwatch = Stopwatch()
 
 	def Close(self):
 		if(not self.IsOpen()):
 			return
 
-		GPIO.output(7, GPIO.HIGH)
+		self._isOpen = False
 		self._stopwatch = None
 
 	def ValveOpenDuration(self):
