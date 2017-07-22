d65d0dc8949cf41ba9e6ebc97ccbd1a87449e806
mcbrown87 <michael.c.brown87@gmail.com>
2017-07-22 09:45:27 -0400

    Added Day schedule criterion

--- scheduler/lawnWateringScheduler.py
+++ scheduler/lawnWateringScheduler.py
@@ -1,18 +1,21 @@
 import datetime
 import time
-from threading import Thread
 
 from scheduler.valveWebService import ValveWebService
 
 class LawnWateringScheduler():
 	def __init__(self):
-		self._duration = datetime.timedelta(0, 0, 0, 0, 2)
+		self._duration = datetime.timedelta(0, 0, 0, 0, 1)
 		self._sleepTime = 30
-		self._scheduler = Scheduler(datetime.time(7, 1))
+		self._runCriteria = [TimeCriterion(datetime.time(9, 34)), DayCriterion(6)]
 		self._valveWebService = ValveWebService()
 
 	def shouldOpen(self):
-		return self._scheduler.shouldStart()
+		for runCriterion in self._runCriteria:
+			if not runCriterion.shouldStart():
+				return False
+
+		return True
 
 	def shouldClose(self):
 		return self._duration < self._valveWebService.OpenDuration()
@@ -27,15 +30,24 @@ class LawnWateringScheduler():
 
 			time.sleep(5)
 
-
-class Scheduler:
-	def __init__(self, startTime=None):
-		self._startTime =  datetime.time(7) if startTime is None else startTime
+class TimeCriterion:
+	def __init__(self, startTime):
+		self._startTime = startTime
 		self._currentTimeGetter = lambda : datetime.datetime.now().time().replace(second=0, microsecond=0)
 
 	def shouldStart(self):
 		return self._startTime == self._currentTimeGetter()
 
+class DayCriterion:
+	def __init__(self, day):
+		if day > 6 or day < 0:
+			raise ValueError("Invalid day value")
+
+		self._day = day
+		self._currentDayGetter = lambda : datetime.datetime.today().weekday()
+
+	def shouldStart(self):
+		return self._day == self._currentDayGetter()
 
 def main():
 	app = LawnWateringScheduler()
--- tests/lawnWateringSchedulerShould.py
+++ tests/lawnWateringSchedulerShould.py
@@ -1,25 +1,29 @@
 import unittest
-import time
 import datetime
 from mock import MagicMock, Mock
-from scheduler.lawnWateringScheduler import LawnWateringScheduler, Scheduler
+from scheduler.lawnWateringScheduler import LawnWateringScheduler, TimeCriterion, DayCriterion
+
 
 class LawnWateringSchedulerShould(unittest.TestCase):
-	def test_open_the_valve_based_on_scheduler(self):
+	def test_open_the_valve_based_on_criteria(self):
 		sut = LawnWateringScheduler()
 
-		mockScheduler = Mock()
-		mockScheduler.shouldStart.return_value = True
-		sut._scheduler = mockScheduler
+		mockCriteria = Mock()
+		mockCriteria.shouldStart.return_value = True
+		sut._runCriteria = [mockCriteria, mockCriteria]
 
 		self.assertTrue(sut.shouldOpen())
 
-	def test_not_open_the_valve_based_on_scheduler(self):
+	def test_not_open_the_valve_based_on_criteria(self):
 		sut = LawnWateringScheduler()
 
-		mockScheduler = Mock()
-		mockScheduler.shouldStart.return_value = False
-		sut._scheduler = mockScheduler
+		mockCriteriaShouldStart = Mock()
+		mockCriteriaShouldStart.shouldStart.return_value = True
+
+		mockCriteriaShouldNotStart = Mock()
+		mockCriteriaShouldNotStart.shouldStart.return_value = False
+
+		sut._runCriteria = [mockCriteriaShouldStart, mockCriteriaShouldNotStart]
 
 		self.assertFalse(sut.shouldOpen())
 
@@ -45,27 +49,45 @@ class LawnWateringSchedulerShould(unittest.TestCase):
 
 		self.assertFalse(sut.shouldClose())
 
-class SchedulerShould(unittest.TestCase):
+class TimeCriterionShould(unittest.TestCase):
 	def test_start_at_start_time(self):
-		sut = Scheduler()
-		sut._startTime = datetime.time(7)
+		sut = TimeCriterion(datetime.time(7))
 		sut._currentTimeGetter = lambda: datetime.time(7)
 
 		self.assertTrue(sut.shouldStart())
 
 	def test_not_start_after_start_time(self):
-		sut = Scheduler()
-		sut._startTime = datetime.time(7, 1)
+		sut = TimeCriterion(datetime.time(7, 1))
 		sut._currentTimeGetter = lambda: datetime.time(7)
 
 		self.assertFalse(sut.shouldStart())
 
 	def test_not_start_before_start_time(self):
-		sut = Scheduler()
-		sut._startTime = datetime.time(6, 59)
+		sut = TimeCriterion(datetime.time(6, 59))
 		sut._currentTimeGetter = lambda: datetime.time(7)
 
 		self.assertFalse(sut.shouldStart())
+
+class DayCriterionShould(unittest.TestCase):
+	def test_fail_with_out_of_range_values_lower_bound(self):
+		with self.assertRaises(ValueError):
+			DayCriterion(-1)
+
+	def test_fail_with_out_of_range_values_upper_bound(self):
+		with self.assertRaises(ValueError):
+			DayCriterion(7)
+
+	def test_not_start_on_off_days(self):
+		sut = DayCriterion(0)
+		sut._currentDayGetter = lambda : 1
+
+		self.assertFalse(sut.shouldStart())
+
+	def test_start_on_on_days(self):
+		sut = DayCriterion(0)
+		sut._currentDayGetter = lambda : 0
+
+		self.assertTrue(sut.shouldStart())
 
 if __name__ == '__main__':
 	unittest.main()
